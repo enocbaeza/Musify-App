1dfb397402a297025c6e9ec1f558d311dfd66dd6
Enoc Baeza <baeza_mz@hotmail.com>
2018-10-30 13:53:17 -0300

    Added User controller and route

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ var bodyParser = require('body-parser');
 //Se crea objeto de express
 var app = express();
 
+//Cargar rutas
+var userRoutes = require('./rourtes/user');
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//Configurar cabeceras http
+
+//Rutas base
+app.use('/api', userRoutes);
 
 module.exports = app;
