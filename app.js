11e657b428c716e927c21a33e674665af566ae2f
Enoc Baeza <baeza_mz@hotmail.com>
2018-11-14 16:05:42 -0300

    Http headers config

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ expressApp.use(bodyParser.urlencoded({extended:false}));
 expressApp.use(bodyParser.json());
 
 //Configurar cabeceras http
+expressApp.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Width, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Request-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    
+    //Sirve para salir de este metodo y seguir con la ejecucion normal
+    next();
+});
 
 //Rutas base
 expressApp.use('/api', userRoutes);
